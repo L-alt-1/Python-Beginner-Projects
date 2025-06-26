f1abf9b80cebf73c85414430482ced7b92d8fa7f
L-alt-1 <muonde.emmie@gmail.com>
2025-06-26 13:40:52 +0200

    Create Project 4.py
    Write a Python program that takes an integer input and prints whether it is positive, negative, or zero.

---
+++ Project 4.py
@@ -0,0 +1,13 @@
+#Declare Variables:
+x= 0
+
+#Input
+x= float(input("Enter a number: "))
+
+#Output
+if x > 0:
+    print("The number is Positive")
+elif x==0:
+    print("The number is Zero")
+else:
+    print("The number is Negative")
