e9b53578d2cf95d20abd3361d80273321e03f1ba
L-alt-1 <muonde.emmie@gmail.com>
2025-06-26 13:38:28 +0200

    Create Project 3.py
    Determine how many pieces of boerewors are left over after distributing them
    evenly among the guests.

---
+++ Project 3.py
@@ -0,0 +1,14 @@
+#Declare variables
+x= 0
+y= 0
+z= 0
+
+#Input
+x= int(input("Enter the total number of boerewors pieces: "))
+y= int(input("Enter the number of guests: "))
+
+#Calculation
+z= x%y
+
+#Output
+print("Pieces of boerewors left over: ", z)
