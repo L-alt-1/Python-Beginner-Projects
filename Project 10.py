7c28041645493eaddd9a4dd56fe30ac93073966d
L-alt-1 <muonde.emmie@gmail.com>
2025-06-26 13:59:11 +0200

    Create Project 10.py
    Write a Python program that includes a user-defined function named guess_number(). Your
    function should first seed the random number generator with the integer literal value of 10 to ensure
    consistent results for automated testing. Next, generate a random integer between 1 and 100
    (inclusive) using the random module. The function should use a Boolean flag to control a while loop
    that continuously prompt the user to guess the randomly generated number. After each guess,
    provide appropriate feedback: inform the user whether the guess was too low or too high. If the user
    guesses correctly, congratulate them and indicate the correct number along with the total number of
    guesses taken. Allow the user to quit the game at any time by entering 0 (zero), in which case display
    a quitting message and terminate the game.

---
+++ Project 10.py
@@ -0,0 +1,42 @@
+#Declare Variables
+test_scores = 0
+total= 0
+count= 0
+Average = 0
+grade= 0
+
+#Function 1
+def calcAverage(total,count):
+    Average= total/5
+    return Average
+
+#Function 2
+def determineGrade(Average):
+    if 90<= Average <=100:
+        grade= "A"
+    elif 80<= Average <=89:
+        grade= "B"
+    elif 70<= Average <=79:
+        grade= "C"
+    elif 60<= Average <=69:
+        grade= "D"
+    else:
+        grade= "F"
+    return grade
+        
+#Output
+def main():
+    count= 1
+    total= 0
+    while count <= 5:
+        test_scores= float(input(f"Enter test score {count}: "))
+        total+= test_scores
+        count+= 1
+    Average= calcAverage(total, 5)
+    grade= determineGrade(Average)
+    print(f"Your average test score is {Average:.2f}% and thus your grade is {grade}")
+
+
+# Demonstration
+if __name__ == "__main__":
+    main()
