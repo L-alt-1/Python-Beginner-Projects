aeb0da10fcf7660d16671560ae246657c7976651
L-alt-1 <muonde.emmie@gmail.com>
2025-06-26 13:52:03 +0200

    Create Project 7.py
    Write a Python script that takes two numbers as input and prints whether the first number is evenly divisible by the second number.

---
+++ Project 7.py
@@ -0,0 +1,13 @@
+#Declare Variables:
+x= 0
+y= 0
+
+#Input
+x= int(input("Enter the first number: "))
+y= int(input("Enter the second number: "))
+
+#Output
+if x%y==0:
+    print(x, "is divisible by", y)
+else:
+    print(x, "is not divisible by", y)
